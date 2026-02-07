173f848138f9b2125d6f5f2d1c64a30677621f05
rerkingali23-hub <r.erkingali23@gmail.com>
2026-02-07 11:37:31 +0500

    ex0

---
+++ Practice 2/boolean/boolean_comparison.py
@@ -0,0 +1,18 @@
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
