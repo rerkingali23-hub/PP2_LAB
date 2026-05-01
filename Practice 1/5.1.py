176596054a936d96b2e28ae6a8d4d42f89a6ae92
rerkingali23-hub <r.erkingali23@gmail.com>
2026-05-02 02:01:25 +0500

    TSIS,practice11

--- Practice 1/5.1.py
+++ Practice 1/5.1.py
@@ -2,3 +2,14 @@ x = 5
 y = "John"
 print(x)
 print(y)
+#legal names:
+#myvar = "John"
+#my_var = "John"
+#_my_var = "John"
+#myVar = "John"
+#MYVAR = "John"
+#myvar2 = "John"
+#illegal names:
+#2myvar = "John"
+#my-var = "John"
+#my var = "John"
